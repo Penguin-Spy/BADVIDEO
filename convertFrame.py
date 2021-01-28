d361f053ed433ab0e6527082cad67a1d63303c20
Penguin_Spy <penguinspy2435@outlook.com>
2021-01-28 01:07:45 -0800

    Video plays at inconsistent 30 fps! Reads the entire frame data into the buffer once, instead of like 300-700 individual reads. Disabled buffering entirely, video looks good enough without it. May change later?

--- convertFrame.py
+++ convertFrame.py
@@ -3,34 +3,42 @@
 # Program Name:    ConvertFrame
 # Author:          Penguin_Spy
 # License:         Use, but don't steal my code thx
-# Description:     Turns a 2-color 320x240 .png into a list describing the image as the length between each color change.
+# Description:     Turns a 2-color 320x240 video into a .bin file describing the image as the length between each color change. Format can be found in format.md
 # --------------------------------------
 #
 
 
 # Largely copy+pasted from dbr's answer to https://stackoverflow.com/questions/1109422/getting-list-of-pixel-values-from-pil
 from PIL import Image
+from tinytag import TinyTag
 import cv2
 import sys
 
 inputPath = sys.argv[1]
 outputPath = sys.argv[2]
 
-
 # Read the video from specified path
 inputVideo = cv2.VideoCapture(inputPath)
+inputVideoTag = TinyTag.get(inputPath)
 
-title = "Bad Apple"
+outputBin = open(outputPath, "wb")
 
-frameByteArray = bytearray("LLV", "ascii")
-frameByteArray.append(0b00000000)   # Version (0=debug)
-frameByteArray.append(0b01000000)   # Features (0 - Captions; 1 - Sound; 2-7 Reserved, should be zeros.)
-frameByteArray.append(0b00000101)   # FPS
-frameByteArray += bytearray(len(title).to_bytes(1, 'big'))   # Title length
-frameByteArray += bytearray(title, "ascii")  # Title
+# title = "Bad Apple"
+title = inputVideoTag.title
+if (title == None):
+    print("[Warning]: Title is empty. Setting to default: 'unknown'")
+    title = "unknown"
+
+headerBytes = bytes("LLVH", "ascii")
+headerBytes += bytes([0b00000000])        # Version (0=debug)
+headerBytes += bytes([0b01000000])   # Features (0 - Captions; 1 - Sound; 2-7 Reserved, should be zeros.)
+headerBytes += bytes([0b00000101])   # FPS
+headerBytes += len(title).to_bytes(1, 'big')   # Title length
+headerBytes += bytes(title, "ascii")  # Title
 # captionCount
 # *captions
-frameCountIndex = len(frameByteArray)  # Frame Count Index (to insert into later)
+
+videoBytes = bytes()
 
 frameCount = 0
 
@@ -40,22 +48,21 @@ while(True):
     ret, frame = inputVideo.read()
 
     if ret:  # Convert to PIL & encode as LLV
-        #cv2.imwrite(''+str(frameCount) + "a.png", frame)
+        # cv2.imwrite(''+str(frameCount) + "a.png", frame)
         color_coverted = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         img = Image.fromarray(color_coverted)
 
-        #img.save(''+str(frameCount) + "b.png")
+        # img.save(''+str(frameCount) + "b.png")
 
         img = img.convert('1', dither=False)  # convert image to 2 color black and white
         img = img.resize((320, 240))  # , box=(0, 0, width, height)
-        #img.save(''+str(frameCount) + "c.png")
+        # img.save(''+str(frameCount) + "c.png")
 
         pixels = img.load()  # this is not a list, nor is it list()'able
         color = pixels[0, 0]
         width, height = img.size
 
-        frameByteArray.append(0b00000000 + ((color == 0) << 7))  # Frame Header
-        lineCountIndex = len(frameByteArray)                    # Line Count Index (to insert into later)
+        frameBytes = bytes()
         currentLineLength = -1  # Start at -1 so that we enter the first pixel at line length 0
         lineCount = 0
 
@@ -67,27 +74,26 @@ while(True):
 
                 # By running both of these checks independently and in this order, we get the desired behavior for lines of exactly 255 (0xFF followed by 0x00).
                 if currentLineLength > 254:     # If we're out of space in this byte, move onto the next one.
-                    frameByteArray.append(currentLineLength)
+                    frameBytes += bytes([currentLineLength])
                     lineCount += 1
                     currentLineLength = 0
                 if not(cpixel == color):        # If the color changed from last pixel
-                    frameByteArray.append(currentLineLength)
+                    frameBytes += bytes([currentLineLength])
                     lineCount += 1
                     currentLineLength = 0
                     color = cpixel
 
         # Save the last line
         # currentLineLength is incremented at beginning of loop, so when we exit we're one pixel behind
-        frameByteArray.append(currentLineLength + 1)
+        frameBytes += bytes([currentLineLength + 1])
         lineCount += 1
 
-        # This is how the TI-84+ CE stores 16 bit uints, so thats how we store it.
-        lineCountBytes = lineCount.to_bytes(2, 'little')
-
-        frameByteArray.insert(lineCountIndex, lineCountBytes[1])
-        frameByteArray.insert(lineCountIndex, lineCountBytes[0])
+        # Frame Header & This is how the TI-84 stores 16 bit ints, so thats how we store it.
+        frameBytes = bytes([(pixels[0, 0] == 0) * 0x80]) + lineCount.to_bytes(2, 'little') + frameBytes
 
         frameCount += 1
+
+        videoBytes += frameBytes
 
         print("Encoded frame", frameCount, "with", lineCount, "lines.")
 
@@ -97,9 +103,6 @@ while(True):
     if(frameCount > 120):
         break
 
-frameCountBytes = frameCount.to_bytes(2, 'little')
-frameByteArray.insert(frameCountIndex, frameCountBytes[1])
-frameByteArray.insert(frameCountIndex, frameCountBytes[0])
+videoBytes = headerBytes + frameCount.to_bytes(2, 'little') + videoBytes
 
-with open(outputPath, "wb") as frame_bin:
-    frame_bin.write(frameByteArray)
+outputBin.write(videoBytes)
--- src/main.c
+++ src/main.c
@@ -7,15 +7,12 @@
  *--------------------------------------
 */
 
-
-
 /* Includes */
 #include <string.h>
 #include <tice.h>
 #include <graphx.h>
 #include <fileioc.h>
 #include <stdlib.h>
-
 
 /*
 |  Version |     0    |  1 Byte  | Version of this LLV file. For this specification, should be (`0b00001001`)
@@ -40,114 +37,106 @@
 |  Caption |  depends |  2 Bytes | If caption bit set, caption index to show. If value is 0xFFFF, clear any displayed captions. Not present otherwise.
 |   Sound  |  depends |  1 Byte? | If sound bit set, tone/note to play. Not present otherwise.
 | #o lines |  depends |  2 Bytes | Number of lines in this frame.
-|Frame Data|  depends | #o lines | List of distances between each color change. A value of 0xFF (255) does not swap the color, allowing contiguous sections of the same color longer than 255. Line lengths of exactly 255 are encoded as a 255 length line, followed by a 0 length line.  
+|Frame Data|  depends | #o lines | List of distances between each color change. A value of 0xFF (255) does not swap the color, allowing contiguous sections of the same color longer than 255. Line lengths of exactly 255 are encoded as a 255 length line, followed by a 0 length line.
 */
 
-    
-typedef struct {
-    char    *text;
+typedef struct
+{
+    char* text;
     uint16_t x;
     uint8_t  y;
     uint8_t  color;
 } caption_t;
 
-typedef struct {
+typedef struct
+{
     uint8_t  header;
     uint8_t  displayTime;
     uint16_t captionIndex;
     uint8_t  sound;
     uint16_t lineCount;
-    uint8_t  frameDataBuffer[255];
 } frame_header_t;
 
-typedef struct {
-    char        fileName[8];
-    uint8_t     version;
-    uint8_t     features;
-    uint8_t     fps;
-    uint8_t     titleLength;
-    char        *title;
-    uint16_t    captionCount;
-    caption_t   *captions;
-    uint16_t    frameCount;
-    frame_header_t   *frameBuffer;
+typedef struct
+{
+    char fileName[8];
+    uint8_t    version;
+    uint8_t    features;
+    uint8_t    fps;
+    uint8_t    titleLength;
+    char* title;
+    uint16_t   captionCount;
+    caption_t* captions;
+    uint16_t   frameCount;
 } llv_header_t;
 
-
 ti_var_t LLV_FILE;
-
-
 
 int main(void)
 {
     llv_header_t LLV_header;
     frame_header_t frame_header;
-    
-    
-    
+
     //uint8_t frame[] = {255,255,255,255,255,190,4,255,59,8,255,57,8,255,56,10,255,55,10,255,55,10,255,55,10,255,56,8,255,57,8,255,59,4,255,255,255,25,4,255,59,8,255,57,8,255,56,10,255,55,10,255,55,10,255,55,10,255,56,8,255,57,8,255,59,4,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,20,4,255,59,8,255,56,9,255,55,11,255,53,12,255,53,12,255,52,13,255,51,13,255,51,14,84,4,217,14,83,8,214,15,83,8,213,14,84,10,211,15,84,10,210,15,85,10,209,15,86,10,208,15,87,10,207,15,88,10,206,15,89,11,204,15,90,11,204,14,91,12,202,14,93,12,200,14,94,12,199,14,96,12,197,14,97,13,195,14,99,13,193,14,100,14,191,15,101,14,189,15,103,14,187,15,104,16,184,15,106,16,182,15,108,16,180,15,110,16,179,14,112,16,177,14,114,17,174,14,116,17,172,14,118,17,170,14,121,16,168,14,123,17,165,14,125,17,163,15,126,17,161,15,128,18,158,15,131,17,156,15,133,17,154,15,135,18,152,14,137,18,150,14,140,17,148,14,142,18,145,14,144,18,143,14,147,18,140,14,149,18,138,14,151,19,135,15,153,18,133,15,155,19,130,15,157,19,128,15,160,19,124,16,162,20,121,16,165,19,119,16,167,20,116,16,170,20,113,16,172,21,109,17,175,21,106,17,177,22,102,18,180,22,99,17,184,23,94,18,186,25,90,18,189,25,86,19,192,26,81,20,195,26,77,20,199,27,73,20,202,28,67,21,206,29,61,23,209,30,56,23,213,32,49,25,217,34,41,26,222,36,32,28,226,43,18,31,231,88,234,84,239,79,244,73,250,67,255,1,62,255,7,55,255,15,47,255,23,38,255,36,24,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255};
     //uint8_t len = sizeof(frame);
-    
-    char fileNames[][10] = {"_______0", "_______1", "_______2", "_______3", "_______4", "_______5", "_______6", "_______7", "_______8", "_______9"};
-    
+
+    char fileNames[][10] = { "_______0", "_______1", "_______2", "_______3", "_______4", "_______5", "_______6", "_______7", "_______8", "_______9" };
+
     uint16_t LLV_SIZE;
-    
-    uint8_t key = 0;
-    uint8_t tempColor = 0;
-    char *var_name;
-    uint8_t *search_pos;
-    uint8_t numFound;
+
+    uint8_t  key = 0;
+    uint8_t  tempColor = 0;
+    char* var_name;
+    uint8_t* search_pos;
+    uint8_t  numFound;
     uint16_t i;
     uint16_t j;
-    
-    
+
+
     uint8_t  frameHeader;
     uint16_t lineCount;
     uint16_t x;
     uint8_t  y;
     uint8_t  VERT_SCALE = 1;
+    uint8_t  FRAME_BYTE_BUFFER_SIZE = 1024;
+    uint8_t  frame[1024]; // help how do i make the buffer size set from the var above, the one above doesn't change
     uint16_t remainingWidth;
-    uint8_t  frame[255];
+    uint8_t  remainingFrameBytes;
     uint8_t  line;
     uint8_t  color = 0;
-    
-    
-    
-    gfx_Begin();            // Initalize graphics
-    
-    
-    
-    
-    
+
+    gfx_Begin(); // Initalize graphics
+
 select:
     // Close any files that may be open already
     ti_CloseAll();
-    
+
     gfx_SetColor(0xFF);
     gfx_FillScreen(74);
-    
+
     search_pos = NULL;
     numFound = 0;
-    
+
     // Pick file
-    while((var_name = ti_DetectVar(&search_pos, "LLV", TI_APPVAR_TYPE)) != NULL) {
+    while ((var_name = ti_DetectVar(&search_pos, "LLVH", TI_APPVAR_TYPE)) != NULL) {
         LLV_FILE = ti_Open(var_name, "r");
-        
+
         // Read data
-        strcpy(LLV_header.fileName, var_name);  // File name for printing
-        strcpy(fileNames[numFound], var_name);      // File name saved for opening
-        ti_Seek(3, SEEK_SET, LLV_FILE);         // Seek past "LLV" header
-        ti_Read(&LLV_header.fileName+8, 4, 1, LLV_FILE);    // Offsets 0-3
-        LLV_header.title = (char*) ti_MallocString(LLV_header.titleLength);
-        ti_Read(LLV_header.title, 1, LLV_header.titleLength, LLV_FILE);    // Title String + null-terminator
-        if((LLV_header.features) & 128) {                   // Captions exist
-            ti_Read(&LLV_header.captionCount, 2, 1, LLV_FILE);      // Number of captions
-            ti_Seek(LLV_header.captionCount, SEEK_CUR, LLV_FILE);   // Seek pask the captions list
+        strcpy(LLV_header.fileName, var_name);             // File name for printing
+        strcpy(fileNames[numFound], var_name);             // File name saved for opening
+        ti_Seek(4, SEEK_SET, LLV_FILE);                    // Seek past "LLVH" header
+        ti_Read(&LLV_header.fileName + 8, 4, 1, LLV_FILE); // Offsets 0-3
+        LLV_header.title = (char*)ti_MallocString(LLV_header.titleLength);
+        ti_Read(LLV_header.title, 1, LLV_header.titleLength, LLV_FILE); // Title String
+        if ((LLV_header.features) & 128)
+        {                                                         // Captions exist
+            ti_Read(&LLV_header.captionCount, 2, 1, LLV_FILE);    // Number of captions
+            ti_Seek(LLV_header.captionCount, SEEK_CUR, LLV_FILE); // Seek pask the captions list
         }
-        ti_Read(&LLV_header.frameCount, 1, 2, LLV_FILE);            // Number of frames
-        
+        ti_Read(&LLV_header.frameCount, 1, 2, LLV_FILE); // Number of frames
+
         // Print in list
-        gfx_SetTextXY(10, numFound*8);
+        gfx_SetTextXY(10, numFound * 8);
         gfx_PrintString(LLV_header.fileName);
         /*gfx_SetTextXY(10+64+10, numFound*8);
         gfx_PrintUInt(LLV_header.version, 8);
@@ -155,9 +144,9 @@ select:
         gfx_PrintUInt(LLV_header.features, 8);
         gfx_SetTextXY(10+64+10+64+10+64+10, numFound*8);
         gfx_PrintUInt(LLV_header.fps, 3);*/
-        gfx_SetTextXY(10+64+10, numFound*8);
+        gfx_SetTextXY(10 + 64 + 10, numFound * 8);
         gfx_PrintString(LLV_header.title);
-        gfx_SetTextXY(10, 8);
+        /*gfx_SetTextXY(10, 8);
         gfx_PrintUInt(LLV_header.version, 8);
         gfx_SetTextXY(10, 16);
         gfx_PrintUInt(LLV_header.features, 8);
@@ -165,105 +154,112 @@ select:
         gfx_PrintUInt(LLV_header.fps, 8);
         gfx_SetTextXY(10, 32);
         gfx_PrintUInt(LLV_header.titleLength, 8);
-        while(!(key = os_GetCSC()));
-        
+        while (!(key = os_GetCSC()))*/
+
         // Close file.
         ti_CloseAll();
-        
+
         numFound++;
-        if(numFound > 10) break;
+        if (numFound > 10)  // We only have enough space for 10 names in the list (TODO: expand list to store a few more, and implement scrolling/whatever through files [search more when scrolling])
+            break;
     }
-    
-    for(i = 0; i < numFound; i++) {
-    }
-    
+
     key = 0;
+    if (numFound == 0) {
+        gfx_SetTextXY(10, 0);
+        gfx_PrintString("No videos found.");
+        while (!(key = os_GetCSC()));
+        key = sk_Clear;
+    }
+
     i = 0;
-    while(key != sk_Enter && key != sk_Clear) {
-        switch(key) {
-            case sk_Up:
-                if(i > 0) i--;
-                break;
-            case sk_Down:
-                if(i < numFound-1) i++;
-                break;
-            // mode for more info?
+    while (key != sk_Enter && key != sk_Clear)
+    {
+        switch (key)
+        {
+        case sk_Up:
+            if (i > 0)
+                i--;
+            break;
+        case sk_Down:
+            if (i < numFound - 1)
+                i++;
+            break;
+            // `mode` for more info?
         }
-        
+
         // Draw selection carrot
         tempColor = gfx_SetColor(74);
-        gfx_FillRectangle(0, 0, 10, 8*numFound);
+        gfx_FillRectangle(0, 0, 10, 8 * numFound);
         gfx_SetColor(tempColor);
-        gfx_SetTextXY(0, i*8);
+        gfx_SetTextXY(0, i * 8);
         gfx_PrintString(">");
-        
-        while(!(key = os_GetCSC()));
+
+        while (!(key = os_GetCSC()));
     }
-    
-    if(key == sk_Clear) {
-        gfx_End();              // End graphics drawing
+
+    if (key == sk_Clear)
+    {
+        gfx_End(); // End graphics drawing
         return 0;
     }
-    
 
     // Open the selected file.
-    LLV_FILE = ti_Open("BADAPPLE", "r");
-    
+    LLV_FILE = ti_Open(fileNames[i], "r");
+
     //ti_Read(&frame, 1, sizeof(frame), LLV_FILE);
-    
-    LLV_SIZE = ti_GetSize(LLV_FILE);
-    
+
+    //LLV_SIZE = ti_GetSize(LLV_FILE);
+
     // Seek to the beginning of the frame data
-    //      LLV, Header, Title string,            frameCount
-    ti_Seek(3    + 4     + LLV_header.titleLength + 2       , SEEK_SET, LLV_FILE);
+    //      LLVH, Header, Title string,            frameCount
+    ti_Seek(4 + 4 + LLV_header.titleLength + 2, SEEK_SET, LLV_FILE);
     gfx_FillScreen(74);
     gfx_SetTextXY(0, 0);
     gfx_PrintUInt(LLV_header.frameCount, 8);
-    
-    while(!(key = os_GetCSC()));
-    
+
+    while (!(key = os_GetCSC()));
+
     /*while(key != sk_Clear) {
-        
+
             ti_Read(&frame, 1, 1, LLV_FILE);
-            
+
             //line = frame[i];
             line = frame[0];
-            
+
         gfx_FillScreen(74);
         gfx_SetTextXY(0, 0);
         gfx_PrintUInt(line, 8);
-        
-        
-        
+
+
+
         while(!(key = os_GetCSC()));
     }*/
-    
-    
-    
-    
-    
-    gfx_SetDrawBuffer();    // Enable buffering (because the screen is fully redrawn each frame)
-    
-    for(i = 0; i < LLV_header.frameCount; i++) {
-        
+
+    //gfx_SetDrawBuffer(); // Enable buffering (because the screen is fully redrawn each frame)
+
+    for (i = 0; i < LLV_header.frameCount; i++)
+    {
+
         ti_Read(&frameHeader, 1, 1, LLV_FILE);
         ti_Read(&lineCount, 1, 2, LLV_FILE);
-        
+
         // draw (320x240)
         x = 0;
         y = 0;
         color = 0x00;
-        gfx_SetColor(0xFF);     // Setup color swapping
-        if(frameHeader & 128) {  // If 1st bit set, start with white
+        gfx_SetColor(0xFF); // Setup color swapping
+        if (frameHeader & 128)
+        { // If 1st bit set, start with white
             color = gfx_SetColor(color);
         }
-    
+
         //while(!(key = os_GetCSC()));
-    
+
         // DEBUG: clear screen before draw.
-            gfx_FillScreen(74);
-            // DEBUG: debug var display
-            /*tempColor = gfx_SetColor(74);
+        //gfx_FillScreen(74);
+        // DEBUG: debug var display
+        /*tempColor = gfx_SetColor(74);
             gfx_FillRectangle(0, 200, 64, 40);
             gfx_SetTextXY(0, 200);
             gfx_PrintUInt(lineCount, 16);
@@ -276,41 +272,61 @@ select:
             gfx_SetTextXY(0, 232);
             gfx_PrintInt(LLV_SIZE, 8);*/
             //gfx_SetColor(tempColor);
-    
-        if(key == sk_Clear || lineCount > 1000) {
+
+        if (key == sk_Clear || lineCount > 1000)
+        {
             return 0;
         }
-        
+
+        remainingFrameBytes = 0;
+        ti_Read(&frame, 1, lineCount, LLV_FILE);
+
         for (j = 0; j < lineCount; j++) {
-            remainingWidth = LCD_WIDTH - x;
-        
-            ti_Read(&frame, 1, 1, LLV_FILE);
-            
+
+            // Load more data if nessecary
+            /*if(remainingFrameBytes < 1) {
+
+                // Calculate how many bytes to load: Frame sizes will not fit into multiples of the frame byte buffer size, and we don't want to read data from the next frame because that messes up the seek cursor pos
+                uint16_t bytesToLoad = lineCount - j;
+                if(bytesToLoad > FRAME_BYTE_BUFFER_SIZE) {  // Clamp to size of byte buffer
+                    bytesToLoad = FRAME_BYTE_BUFFER_SIZE;
+                }
+
+                ti_Read(&frame, FRAME_BYTE_BUFFER_SIZE, 1, LLV_FILE);
+                remainingFrameBytes = bytesToLoad;  // Keep track of how many bytes are loaded.
+            }*/
+
             //line = frame[j];
-            line = frame[0];
-        
-            if(remainingWidth < line) {
+            line = frame[j];
+
+            remainingWidth = LCD_WIDTH - x;
+
+            if (remainingWidth < line) {
                 // line for remainingWidth
-                gfx_FillRectangle(x, y, remainingWidth, VERT_SCALE);
-            
+                //gfx_FillRectangle(x, y, remainingWidth, VERT_SCALE);
+                gfx_HorizLine_NoClip(x, y, remainingWidth);
+
                 // line for line - remainingWidth on next line
-                gfx_FillRectangle(0, y + VERT_SCALE, line - remainingWidth, VERT_SCALE);
-            
+                //gfx_FillRectangle(0, y + VERT_SCALE, line - remainingWidth, VERT_SCALE);
+                gfx_HorizLine_NoClip(0, y + VERT_SCALE, line - remainingWidth);
+
                 // loop x and +1 to y
                 x = line - remainingWidth;
                 y += VERT_SCALE;
-            } else {
+            }
+            else {
                 // line for line
-                gfx_FillRectangle(x, y, line, VERT_SCALE);
+                //gfx_FillRectangle(x, y, line, VERT_SCALE);
+                gfx_HorizLine_NoClip(x, y, line);
+
                 // increase x offset by length of line
                 x += line;
             }
-        
-            if(line != 255) {   // Swap color, unless line length was exactly 255 (0xFF). This is to allow contiguous sections of the same color longer than 255.
-                color = gfx_SetColor(color);    // To display a line of length 255, encode it as a 255 length line (0xFF) followed by a 0 length line (0x00).
+
+            if (line != 255) {                                // Swap color, unless line length was exactly 255 (0xFF). This is to allow contiguous sections of the same color longer than 255.
+                color = gfx_SetColor(color); // To display a line of length 255, encode it as a 255 length line (0xFF) followed by a 0 length line (0x00).
             }
-        
-            
+
             // DEBUG: debug var display
             /*tempColor = gfx_SetColor(74);
             gfx_FillRectangle(0, 200, 64, 40);
@@ -325,21 +341,21 @@ select:
             gfx_SetTextXY(0, 232);
             gfx_PrintInt(y, 8);
             gfx_SetColor(tempColor);*/
-            
-            
+
+            remainingFrameBytes--;
         }
-        
+
         /*tempColor = gfx_SetColor(74);
         gfx_FillRectangle(0, 200, 64, 40);
         gfx_SetTextXY(0, 200);
         gfx_PrintString("for loop complete");
         gfx_SetColor(tempColor);*/
-        gfx_SwapDraw();
-        
+        //gfx_SwapDraw();
+
         //if(key == sk_Clear) break;      // DEBUG: quit on clear pressed
     }
-    
+
     key = 0;
-    gfx_SetDrawScreen();
+    //gfx_SetDrawScreen();
     goto select;
 }
